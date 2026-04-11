f22bfeb600fea6cc2d01d8111ffe234dab3b5214
boris <barys.serzhanovich@gmail.com>
2026-04-11 21:20:17 +0300

    Initial Commit

---
+++ FoodTracker/FoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerApp.swift
+//  FoodTracker
+//
+//  Created by Boris Serzhanovich on 11.04.26.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
